6844d760fe3532b92d420d8a15120ca744e009b9
matthewtmoran <matthewtmoran@gmail.com>
2016-09-15 17:52:16 -0500

    test

---
+++ main.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function () {
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/client/index.html');
+
+    // Open the devtools.
+    mainWindow.openDevTools();
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+
+});
