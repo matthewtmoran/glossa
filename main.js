a513c85fa6ca08940924a947915d6e7e1871d794
onechrisjones <onechrisjones@me.com>
2016-10-05 12:26:37 +1000

    Got rid of the macOS menu bar!

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
 
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden'});
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/client/index.html');
