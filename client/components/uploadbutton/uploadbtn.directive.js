909e50238cb000e52b040431c6172948d0c972e5
matthewtmoran <matthewtmoran@gmail.com>
2016-10-19 17:08:45 -0500

    some more clean up

--- client/components/uploadbutton/uploadbtn.directive.js
+++ client/components/uploadbutton/uploadbtn.directive.js
@@ -20,29 +20,21 @@ function apsUploadFile(fileSrvc) {
 
         var input = angular.element(element[0].querySelector('#fileInput'));
         var button = angular.element(element[0].querySelector('#uploadButton'));
-        // var textInput = angular.element(element[0].querySelector('#textInput'));
 
         if (input.length && button.length) {
-            // if (input.length && button.length && textInput.length) {
             button.click(function (e) {
                 input.click();
             });
-            // textInput.click(function (e) {
-            //     input.click();
-            // });
         }
 
         input.on('change', function (e) {
             var files = e.target.files;
-            console.log('files', files);
             if (files[0]) {
                 scope.fileName = files[0].name;
             } else {
                 scope.fileName = null;
             }
-
             fileSrvc.attachFile(files);
-            // nodeSrvc.addFiles(files);
             scope.$apply();
         });
     }
