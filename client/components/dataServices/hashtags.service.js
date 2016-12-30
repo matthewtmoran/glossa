1579348dc6f0903797fc34d6fcc7e37fd48c56fb
matthewtmoran <matthewtmoran@gmail.com>
2016-12-30 15:48:28 -0600

    documentation on hashtag.service

--- client/components/dataServices/hashtags.service.js
+++ client/components/dataServices/hashtags.service.js
@@ -13,24 +13,31 @@ angular.module('glossa')
 
 function hashtagSrvc(dbSrvc, $q) {
     var service = {
-        searchHastags: searchHastags,
-        updateTag: updateTag,
-        createHashtag: createHashtag,
+        query: query,
+        termQuery: termQuery,
+        update: update,
+        save: save,
         normalizeHashtag: normalizeHashtag,
-        get: get,
         removeHashtag: removeHashtag
     };
 
     return service;
     ///////////////
 
-    function get() {
-        return dbSrvc.find(hashtagsCol, {}).then(function(docs) {
+    /**
+     * Queries all hashtags
+     */
+    function query() {
+        return dbSrvc.termQuery(hashtagsCol, {}).then(function(docs) {
             return docs;
         })
     }
 
-    function searchHastags(term) {
+    /**
+     * Searches by term
+     * @param term
+     */
+    function termQuery(term) {
         var query = {
             tag:  new RegExp('^' + term + '$', 'i')
         };
@@ -39,13 +46,22 @@ function hashtagSrvc(dbSrvc, $q) {
         })
     }
 
-    function updateTag(objectToUpdate) {
+    /**
+     * Update Existing tag
+     * @param objectToUpdate
+     * @returns {*}
+     */
+    function update(objectToUpdate) {
         return dbSrvc.basicUpdate(hashtagsCol, objectToUpdate)
     }
 
-    function createHashtag(term) {
+    /**
+     * Save new term as hashtag
+     * @param term
+     */
+    function save(term) {
         var hashtag = {
-            tag: term,
+            tag: term
         };
 
         hashtag.tagColor = '#4285f4';
@@ -58,23 +74,25 @@ function hashtagSrvc(dbSrvc, $q) {
         });
     }
 
+    /**
+     * Normalizes hashtags accross application
+     * @param tag
+     */
     function normalizeHashtag(tag) {
-
+        //store pormises
         var promises = [];
 
+        //Save transcription file promise
         var filePromise = dbSrvc.find(fileCollection, {"hashtags._id": tag._id}).then(function(result) {
 
             result.data.forEach(function(file, i) {
-
                 //Modify the data
                 file.hashtags.forEach(function(t, index) {
                     if (t._id === tag._id) {
                         file.hashtags[index] = tag;
                     }
                 });
-
                return dbSrvc.basicUpdate(fileCollection, file);
-
             });
         });
 
@@ -88,21 +106,25 @@ function hashtagSrvc(dbSrvc, $q) {
                         nb.hashtags[index] = tag;
                     }
                 });
-
                 return dbSrvc.basicUpdate(nbCollection, nb);
             });
-
         });
 
+        //push promises to promise array
         promises.push(filePromise);
         promises.push(notebookPromise);
 
         return $q.all(promises).then(function(result) {
             return result;
         });
-
     }
 
+    /**
+     * Removes hashtag
+     * Should normalize across application
+     * @param tag
+     * @returns {*}
+     */
     function removeHashtag(tag) {
         var promises = [];
 
@@ -111,6 +133,7 @@ function hashtagSrvc(dbSrvc, $q) {
         var notebookPromise = dbSrvc.find(nbCollection, {"hashtags._id": tag._id}).then(function(result) {
             var re = new RegExp('#'+ tag.tag, "gi");
             result.data.forEach(function(nb, i) {
+                //remove the # character from instances
                 nb.description = nb.description.replace(re, tag.tag);
                 //Modify the data
                 nb.hashtags.forEach(function(t, index) {
@@ -128,6 +151,7 @@ function hashtagSrvc(dbSrvc, $q) {
         var filePromise = dbSrvc.find(fileCollection, {"hashtags._id": tag._id}).then(function(result) {
             var re = new RegExp('#'+ tag.tag, "gi");
             result.data.forEach(function(file, i) {
+                //remove hash characters from instances
                 file.description = file.description.replace(re, tag.tag);
                 //Modify the data
                 file.hashtags.forEach(function(t, index) {
@@ -148,7 +172,7 @@ function hashtagSrvc(dbSrvc, $q) {
         return $q.all(promises).then(function(result) {
             return result;
         }).catch(function(err) {
-            console.log('there was an error ', err);
+            console.log('Error removing tags', err);
         });
     }
 }
