f8304281601168bc037d5fd807f81ba33aa4abb3
matthewtmoran <matthewtmoran@gmail.com>
2017-07-25 11:31:41 -0500

    some clean up and documentation in simplemdecomponent

--- src/app/components/simplemde/simplemde.component.js
+++ src/app/components/simplemde/simplemde.component.js
@@ -17,9 +17,6 @@ export const simplemdeComponent = {
       'ngInject';
       this.$timeout = $timeout;
       this.$element = $element;
-
-
-
     }
 
     $onChanges(changes) {
@@ -46,12 +43,6 @@ export const simplemdeComponent = {
       this.isLoading = true;
       this.hashReg = new RegExp("(^|\s)(#[a-z\d-]+)", "i");
       //TODO: should probably change data structure or hint source code
-      this.hashtagList = this.hashtags.map((tag) => {
-        tag.displayText = tag.tag;
-        tag.text = tag.tag;
-        return tag;
-      });
-
       //timeout here so angular digest cycle is valid
       this.$timeout(() => {
         this.mde = new SimpleMDE(this.editorOptions);
@@ -62,7 +53,9 @@ export const simplemdeComponent = {
         this.editor.on('change', this.changeEvent.bind(this));
         this.editor.on('blur', this.blurEvent.bind(this));
 
-
+        //this is our little hint widget
+        //defined here so we can potentially move back to the original source code
+        //TODO: consider using original source code rather than custom fork
         this.cm.registerHelper('hint', 'hashtagHints', (editor) => {
           let cur = editor.getCursor(), curLine = editor.getLine(cur.line);
           let start = cur.ch, end = start;
@@ -70,23 +63,26 @@ export const simplemdeComponent = {
           while (start && /[\w-#]+/.test(curLine.charAt(start - 1))) --start;
           let curWord = start != end && curLine.slice(start, end); //this includes hash
           let tag;
-
           if (curWord) {
             tag = curWord.charAt(0) === '#' ? curWord.slice(1) : '';
           }
+          //TODO: might want to change for more specific regex for accuracy
           let regex = new RegExp('^' + tag, 'i'); //this is just the word
           let result = {
             list: (!tag ? [] : this.hashtags.filter((item) => {
               if (!item.tagDescription) {
                 item.tagDescription = '';
               }
+             // looking for whatever is typed in certain parameters in taglist
              return item.tag.toLowerCase().indexOf(tag.toLowerCase()) > -1 || item.tagDescription.toLowerCase().indexOf(tag.toLowerCase()) > -1
             })).sort(),
             from: this.cm.Pos(cur.line, start),
             to: this.cm.Pos(cur.line, end)
           };
           if (result) {
+            //wrapper for pick event; customized for our needs
             this.cm.on(result, "pick", (item) => {
+              //get the length of the tag
               let lengthOfTag = {line:result.to.line, ch: item.tag.length + 1 }; //the length of the tag plus the # char
               editor.replaceRange('#' + item.tag + ' ', result.from, lengthOfTag); //replace the tag with the # char plus a space to close the hint
             });
@@ -94,10 +90,6 @@ export const simplemdeComponent = {
           return result;
         });
       });
-
-
-
-
 
     }
 
@@ -138,68 +130,14 @@ export const simplemdeComponent = {
     }
 
 
-
-    //hashtags and hints
+    //triggered when change occurs
     showHashtagHints(instance, object) {
-      console.log('this.editor.hasFocus()', this.editor.hasFocus());
+      //show if editor is focused and origin is not complete
       if (this.editor.hasFocus() && object.origin !== 'complete') {
         instance.showHint({ hint: this.cm.hint.hashtagHints, completeSingle: false, closeOnUnfocus: true });
       }
-
-
-
-
-      // let cursor = instance.getCursor(); //gets the cursor
-      // let currentLine = instance.getLine(cursor.line); //gets the line where the cursor is
-      // let start = cursor.ch; //save the start of the cursor
-      // let end = start; // start and end are the same
-      //
-      //
-      // while (end < currentLine.length && /[\w#]+/.test(currentLine.charAt(end))) ++end;
-      // while (start && /[\w#]+/.test(currentLine.charAt(start - 1))) --start;
-      // let curWord = start !== end && currentLine.slice(start, end);
-      //
-      //
-      // console.log('curWord:', curWord);
-
-
-      // let regex = new RegExp('^' + curWord, 'i');
-      //
-      //
-      //
-      // let result = {
-      //   list: (!curWord ? this.hashtagList : this.hashtagList.filter(function (item) {
-      //     return item.match(regex);
-      //   })).sort(),
-      //   from: this.cm.Pos(cursor.line, start),
-      //   to: this.cm.Pos(cursor.line, end)
-      // };
-      //
-      // return result;
-
-
-      //if it's a hashtag this gets called
-      // if (this.hashReg.test(curWord)) {
-      //   //I'm binding the list here and passing it to codemirror
-      //   instance.list = this.hashtagList;
-      //   //calls this custom function that's defined in codemirror.  Definitley need that option otherwise we will end up in an endless loop
-      //
-      //   this.cm.showHint(instance, this.cm.hint.customList, {
-      //     "completeSingle": false,
-      //     'closeOnUnfocus': true //
-      //   })
-      // }
     };
 
-    //I'm not sure we will use wave-preview at all
-    rerenderPreview(val) {
-      console.log("rendering wave-preview");
-    };
-
-    rerenderPreview() {
-
-    }
   }
 
-
-}
+};
