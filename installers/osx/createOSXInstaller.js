00825a939b2d227ff84febd7194f03d4020d1672
matthewtmoran <matthewtmoran@gmail.com>
2017-08-15 11:11:56 -0500

    removed online status text

--- installers/osx/createOSXInstaller.js
+++ installers/osx/createOSXInstaller.js
@@ -1,37 +1,19 @@
 const createDMG = require('electron-installer-dmg');
 const path = require('path');
+const rootPath = path.join('./');
+const outPath = path.join(rootPath, 'release-builds');
 
-getInstallerConfig()
-  .then(createDMG)
-  .catch((error) => {
-    console.error(error.message || error);
-    process.exit(1)
-  });
+const opts = {
+  appPath: path.join(outPath, 'Glossa-darwin-x64/Glossa.app'),
+  name:'Glossa',
+  icon:  path.join(__dirname, './glossa-logo.png'),
+  overwrite: true,
+  out: path.join(outPath, 'osx-installer')
+};
 
-function getInstallerConfig() {
-  console.log('creating osx installer');
+createDMG(opts, function done (err) {
+  if (err) {
+    console.log('Error making osx dmg', err)
+  }
+})
 
-  const rootPath = path.join('./');
-  const outPath = path.join(rootPath, 'release-builds');
-
-  console.log(path.join(__dirname, './glossa-logo.ico'));
-
-  return Promise.resolve({
-    name:'Glossa',
-    icon:  path.join(__dirname, './glossa-logo.png'),
-    overwrite: true,
-    out: path.join(outPath, 'osx-installer'),
-    appPath: path.join(outPath, 'Glossa-darwin-x64/Glossa.app')
-    // appDirectory: path.join(outPath, 'Glossa-win32-ia32'),
-    // authors: 'Chris Jones, Justin Rees, Matthew Moran',
-    // noMsi: true,
-    // exe: 'Glossa.exe',
-    // description: 'Glossa Application for CLA',
-    // setupExe: 'GlossaInstaller.exe',
-    // iconUrl: path.join(__dirname, './glossa-logo.ico'),
-    // setupIcon: path.join(__dirname, './glossa-logo.ico'),
-  })
-}
-
-
-// createDMG(opts, function done (err) { })
